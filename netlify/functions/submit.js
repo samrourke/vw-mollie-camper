8cb444c40cdf818157bf789d42f489ebfaad861f
Sam Rourke <samrourke@gmail.com>
2026-05-22 09:58:45 +0100

    Fix: netlify functions submit.js added

---
+++ netlify/functions/submit.js
@@ -0,0 +1,17 @@
+export default async (req, context) => {
+  const body = await req.text();
+
+  const response = await fetch("https://vw-mollie.netlify.app/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body,
+  });
+
+  return new Response(null, { status: 200 });
+};
+
+export const config = {
+  path: "/submit",
+};
